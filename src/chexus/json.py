b5f79d8021f1e0e455224a3cf370ca69f63ac017
Johannes Kasimir <johanneskasimir@gmail.com>
2026-07-02 14:44:27 +0200

    feat: make json loader read dtypes correctly and also load smaller json values for validation

--- src/chexus/json.py
+++ src/chexus/json.py
@@ -1,11 +1,22 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 import json
+import sys
 from typing import Any
 
 import numpy as np
 
 from .tree import Dataset, Group
+
+_MAX_STORED_DATASET_VALUE_SIZE = 100 * 1024
+
+
+class _DatasetWasTooBig:
+    def __repr__(self) -> str:
+        return "dataset_was_too_big"
+
+
+dataset_was_too_big = _DatasetWasTooBig()
 
 
 def read_json(path: str) -> Group:
@@ -46,7 +57,7 @@ def read_json(path: str) -> Group:
                 "config": {
                     "name": "slits",
                     "values": 1,
-                    "type": "int64"
+                    "dtype": "int64"
                 }
             },
         ]
@@ -81,16 +92,51 @@ def _read_group(group: dict[str, Any], parent: Group | None = None) -> Group:
 
 def _read_dataset(dataset: dict[str, Any], parent: Group) -> Dataset:
     """Read JSON dataset"""
-    name = parent.name + '/' + dataset['config']["name"]
-    if (values := dataset["config"].get("values")) is not None:
-        type_from_values = type(values)
+    config = dataset["config"]
+    name = parent.name + '/' + config["name"]
+    if "dtype" in config:
+        dtype = _translate_dtype(config["dtype"])
+    elif "values" in config:
+        dtype = np.dtype(type(config["values"]))
+    else:
+        dtype = None
+
+    kwargs = {}
+    if "values" in config:
+        if _json_value_fits(config["values"], _MAX_STORED_DATASET_VALUE_SIZE):
+            kwargs["value"] = config["values"]
+        else:
+            kwargs["value"] = dataset_was_too_big
+
     return Dataset(
         name=name,
         shape=None,
-        dtype=_translate_dtype(dataset["config"].get("type", type_from_values)),
+        dtype=dtype,
         attrs=_read_attrs(dataset),
         parent=parent,
+        **kwargs,
     )
+
+
+def _json_value_fits(value: Any, limit: int) -> bool:
+    if (size := _fast_json_value_size(value, limit)) is not None:
+        return size < limit
+    return sys.getsizeof(json.dumps(value)) < limit
+
+
+def _fast_json_value_size(value: Any, limit: int) -> int | None:
+    if value is None or isinstance(value, str | int | float | bool):
+        return sys.getsizeof(value)
+    if not isinstance(value, list):
+        return None
+    size = sys.getsizeof(value)
+    for item in value:
+        if size >= limit:
+            break
+        if (item_size := _fast_json_value_size(item, limit - size)) is None:
+            return None
+        size += item_size
+    return size
 
 
 def _read_source(source: dict[str, Any], parent: Group) -> Dataset:
